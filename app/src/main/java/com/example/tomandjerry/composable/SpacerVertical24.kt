7501cf313b335e7f061432ca1032dc74041335c1
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-06-05 01:44:14 +0300

    Rename SpacerVertical24.kt and update package structure

--- app/src/main/java/com/example/tomandjerry/composable/SpacerVertical24.kt
+++ app/src/main/java/com/example/tomandjerry/composable/SpacerVertical24.kt
@@ -1,8 +1,7 @@
-package com.example.tomandjerry
+package com.example.tomandjerry.composable
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
