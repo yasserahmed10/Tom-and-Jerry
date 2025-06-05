6e56de259c1077ea51db98abd5efaff5cf629bb3
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-06-05 19:40:07 +0300

    Add preview function for JerryStore composable

--- app/src/main/java/com/example/tomandjerry/screen/JerryStore.kt
+++ app/src/main/java/com/example/tomandjerry/screen/JerryStore.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tomandjerry.R
 import com.example.tomandjerry.composable.SpacerVertical24
@@ -136,3 +137,8 @@ fun JerryStore() {
     }
 }
 
+@Preview()
+@Composable
+fun JerryStorePreview(){
+    JerryStore()
+}
