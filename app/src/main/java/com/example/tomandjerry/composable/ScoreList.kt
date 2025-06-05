9b0bc286558b02d780c0dfc6c3e0a87ddf3cfc28
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-06-05 19:34:02 +0300

    Remove unused imports in ScoreList.kt

--- app/src/main/java/com/example/tomandjerry/composable/ScoreList.kt
+++ app/src/main/java/com/example/tomandjerry/composable/ScoreList.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.tomandjerry.screen.Score
-import com.example.tomandjerry.screen.ScoreCard
 
 @Composable
 fun ScoreList(scores: List<Score>) {
