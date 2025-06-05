aca4b88d6464eed9a969f0d32815db25b40f3969
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-06-05 20:03:57 +0300

    Add SpacerHorizontal8 import to Setting.kt for layout consistency

--- app/src/main/java/com/example/tomandjerry/composable/Setting.kt
+++ app/src/main/java/com/example/tomandjerry/composable/Setting.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.tomandjerry.spacer.SpacerHorizontal8
 import com.example.tomandjerry.ui.theme.IBM
 
 @Composable
