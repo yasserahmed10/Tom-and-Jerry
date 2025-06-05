783c3a5e86fbadc366da099ac78f6935fc5fa716
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-06-05 20:04:15 +0300

    Add SpacerVertical12 and SpacerVertical8 imports to SettingList.kt for layout enhancements

--- app/src/main/java/com/example/tomandjerry/composable/SettingList.kt
+++ app/src/main/java/com/example/tomandjerry/composable/SettingList.kt
@@ -8,6 +8,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.tomandjerry.spacer.SpacerVertical12
+import com.example.tomandjerry.spacer.SpacerVertical8
 
 @Composable
 fun SettingList(settingSections: List<SettingSection>) {
