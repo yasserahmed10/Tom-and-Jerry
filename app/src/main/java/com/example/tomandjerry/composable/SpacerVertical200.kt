9ffe37cd877564c8b6b111ac0df5ae6c50e64644
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-06-05 18:45:02 +0300

    Add SpacerVertical200 composable with specified height and background color

---
+++ app/src/main/java/com/example/tomandjerry/composable/SpacerVertical200.kt
@@ -0,0 +1,20 @@
+package com.example.tomandjerry.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpacerVertical200(){
+    Spacer(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .background(Color(0xFF226993))
+    )
+}
