40d5d0ec8d9be3ffb685b7c184cc55476d21e60a
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-06-05 20:08:08 +0300

    Rename SpacerHorizontal24.kt package for improved organization

--- app/src/main/java/com/example/tomandjerry/spacer/SpacerHorizontal24.kt
+++ app/src/main/java/com/example/tomandjerry/spacer/SpacerHorizontal24.kt
@@ -1,4 +1,4 @@
-package com.example.tomandjerry.composable
+package com.example.tomandjerry.spacer
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
