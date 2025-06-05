a5bcf31d3b8455f8a0c062ce030718ba44183ccc
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-06-05 19:13:39 +0300

    Add ScoreList composable for displaying a list of scores

---
+++ app/src/main/java/com/example/tomandjerry/composable/ScoreList.kt
@@ -0,0 +1,24 @@
+package com.example.tomandjerry.composable
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.tomandjerry.screen.Score
+import com.example.tomandjerry.screen.ScoreCard
+
+@Composable
+fun ScoreList(scores: List<Score>) {
+    for (i in 0..scores.lastIndex step 2) {
+        Row(modifier = Modifier.padding(horizontal = 16.dp)) {
+            ScoreCard(scores[i])
+            if (i <= scores.lastIndex) {
+                SpacerHorizontal8()
+                ScoreCard(scores[i + 1])
+            }
+        }
+        if (i <= scores.lastIndex / 2)
+            SpacerVertical8()
+    }
+}
