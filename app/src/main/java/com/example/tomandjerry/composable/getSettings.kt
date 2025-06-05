6d20255d4fd249b3a34ff1a583cc09373808e355
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-06-05 19:19:27 +0300

    Add ScoreCard composable for displaying individual scores

---
+++ app/src/main/java/com/example/tomandjerry/composable/getSettings.kt
@@ -0,0 +1,26 @@
+package com.example.tomandjerry.composable
+
+import com.example.tomandjerry.R
+import com.example.tomandjerry.screen.Setting
+import com.example.tomandjerry.screen.SettingSection
+
+fun getSettings(): List<SettingSection> {
+    return listOf(
+        SettingSection(
+            title = "Tom settings",
+            settings = listOf(
+                Setting("Change sleeping place", R.drawable.bed_single_02),
+                Setting("Meow settings", R.drawable.cat),
+                Setting("Password to open the fridge", R.drawable.fridge),
+            )
+        ),
+        SettingSection(
+            "His favorite foods",
+            listOf(
+                Setting("Mouses", R.drawable.alert_01),
+                Setting("Last stolen meal", R.drawable.hamburger_02),
+                Setting("Change sleep mood", R.drawable.sleeping),
+            )
+        )
+    )
+}
