d4f717fdde6970c0bd3dffe6dd04f8f3c44ac424
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-06-05 18:56:49 +0300

    Add TomAccountBody composable for displaying scores and settings

---
+++ app/src/main/java/com/example/tomandjerry/composable/TomAccountBody.kt
@@ -0,0 +1,35 @@
+package com.example.tomandjerry.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.tomandjerry.screen.Score
+import com.example.tomandjerry.screen.ScoreList
+import com.example.tomandjerry.screen.SettingList
+import com.example.tomandjerry.screen.SettingSection
+import com.example.tomandjerry.ui.theme.BackgroundColor
+
+@Composable
+fun TomAccountBody(
+    scores: List<Score>,
+    settingSections: List<SettingSection>
+) {
+    Column(
+        modifier = Modifier
+            .background(
+                BackgroundColor,
+                RoundedCornerShape(topEnd = 20.dp, topStart = 20.dp)
+            )
+    ) {
+        SpacerVertical23()
+
+        ScoreList(scores)
+
+        SpacerVertical24()
+
+        SettingList(settingSections)
+    }
+}
